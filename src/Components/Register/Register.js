88dfef964fb761fbae32ac8d775063dd1513b704
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-09-14 15:11:10 +0300

    Добавляет проверку паролей при регистрации

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,52 +1,16 @@
 import s from './Register.module.css';
 import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
+import PasswordChecklist from 'react-password-checklist';
 import { v4 as uuidv4 } from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
 import { getSubmitData } from '../../redux/contacts-operations';
 import { toast } from 'react-toastify';
 
 export function Register() {
-    // const dispatch = useDispatch();
+    const [password, setPassword] = useState('');
+    const [passwordAgain, setPasswordAgain] = useState('');
 
-    // const [name, setName] = useState('');
-    // const [number, setNumber] = useState('');
-
-    // const handleClick = event => {
-    //     const { value, name } = event.target;
-
-    //     switch (name) {
-    //         case 'name':
-    //             setName(value.trim());
-    //             break;
-    //         case 'number':
-    //             setNumber(value);
-    //             break;
-    //         default:
-    //             return;
-    //     }
-    // };
-
-    // const contact = useSelector(({ contacts }) =>
-    //     contacts.map(({ name }) => name),
-    // );
-
-    // const handleSubmit = event => {
-    //     event.preventDefault();
-    //     if (contact.includes(name.toLowerCase())) {
-    //         return toast.warning('Such a name already exists!', {
-    //             theme: 'dark',
-    //         });
-    //     }
-
-    //     dispatch(getSubmitData({ name: name, number: number }));
-    //     resetState();
-    // };
-
-    // const resetState = () => {
-    //     setName('');
-    //     setNumber('');
-    // };
+    console.log(password);
 
     return (
         <form
@@ -74,8 +38,6 @@ export function Register() {
                     type="email"
                     className={s.input}
                     name="email"
-                    // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    // title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                     required
                     // onChange={handleClick}
                     // value={number}
@@ -89,14 +51,38 @@ export function Register() {
                     type="password"
                     className={s.input}
                     name="password"
-                    // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    // title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                     required
-                    // onChange={handleClick}
-                    // value={number}
+                    onChange={e => setPassword(e.target.value)}
                     id={uuidv4()}
                 />
             </label>
+
+            <label>
+                <span className={s.name}>Password Again</span>
+                <input
+                    type="password"
+                    className={s.input}
+                    name="password"
+                    required
+                    onChange={e => setPasswordAgain(e.target.value)}
+                    id={uuidv4()}
+                />
+            </label>
+
+            <PasswordChecklist
+                rules={[
+                    'minLength',
+                    'specialChar',
+                    'number',
+                    'capital',
+                    'match',
+                ]}
+                minLength={5}
+                iconSize={8}
+                value={password}
+                valueAgain={passwordAgain}
+                onChange={isValid => {}}
+            />
 
             <button type="submit" className={s.button}>
                 Register
